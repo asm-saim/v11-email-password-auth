8330310bd6a7357e465c8f9b37e03622e9588b6d
asm-saim <saim777m@gmail.com>
2026-08-12 16:44:22 +0600

    login error message implemented

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,70 @@
-import React from 'react';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
+import { Link } from "react-router";
+import { auth } from "../../Firebase/firebase.init";
 
 const Login = () => {
-    return (
-        
-    );
+  const [error, setError] = useState("");
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    console.log(" print", email, password);
+
+    //reset error message:
+    setError("");
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((error) => {
+        console.log(error.message);
+        setError(error.message)
+      });
+  };
+
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <form onSubmit={handleLogin}>
+              <fieldset className="fieldset">
+                <label className="label">Email</label>
+                <input
+                  type="email"
+                  className="input"
+                  name="email"
+                  placeholder="Email"
+                />
+                <label className="label">Password</label>
+                <input
+                  type="password"
+                  className="input"
+                  placeholder="Password"
+                  name="password"
+                />
+                <div>
+                  <a className="link link-hover">Forgot password?</a>
+                </div>
+                <button className="btn btn-neutral mt-4">Login</button>
+              </fieldset>
+              {error && <p className="text-red-500">{error}</p>}
+            </form>
+            <p>
+              New in the website? Please{" "}
+              <Link className="text-blue-500 underline" to="/register">
+                Register
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
